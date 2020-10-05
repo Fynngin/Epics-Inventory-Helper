fdeacf4e109ed336f62130926f99d01acef9e237
Fynn Knoop <fknoop@techfak.uni-bielefeld.de>
2020-10-06 00:12:31 +0200

    Finished logic, need to implement coin image

--- background.js
+++ background.js
@@ -1,15 +1,15 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if (changeInfo.url && changeInfo.url.match("http(s)?://app.epics.gg/(csgo|streamers)/trading/select/[0-9]+/(in|out)")) {
+    if (changeInfo.url && changeInfo.url.match("http(s)?://app.epics.gg/(csgo|streamers)/trading/select/[0-9]+/(in|out)\\?collection=[0-9]+")) {
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+
+            let categoryId = changeInfo.url.match("(csgo|streamers)")[0] === 'csgo' ? 1 : 2
+            let collectionId = changeInfo.url.match("collection=[0-9]+")[0].match(/(\d+)/)[0]
+
             chrome.tabs.sendMessage(tabId, {
-                message: 'trade'
+                message: 'trade',
+                collectionId: collectionId,
+                categoryId: categoryId
             })
         });
     }
 });
-
-chrome.webRequest.onCompleted.addListener(function(details) {
-    if (details.type) {
-        console.log(details)
-    }
-}, {urls: ["<all_urls>"]});
--- content.js
+++ content.js
@@ -1,31 +1,68 @@
+let jwt = window.localStorage.getItem("new:jwt:token")
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if (request.message === 'trade') {
-        getCardTemplates();
+        getCardTemplates(request.categoryId, request.collectionId, jwt)
     }
 });
 
-function getCardTemplates() {
-    var observer = new MutationObserver(function(mutations) {
-        mutations.forEach(function(mutation) {
-            if (!mutation.addedNodes) return
-            let cards = Array.from(mutation.addedNodes).filter(el => el.tagName === 'LI' && el.firstChild.tagName === 'DIV');
-            if (cards.length > 0) {
-                for (let card of cards) {
-                    getMarketValues(card.firstChild.firstChild.firstChild.firstChild.src)
-                }
+function getCardTemplates(categoryId, collectionId, jwt) {
+    let url = `https://api.epics.gg/api/v1/collections/${collectionId}/card-templates?categoryId=${categoryId}`
+    fetch(url, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-User-JWT': jwt
+        },
+    }).then(res => res.json()).then(data => {
+        let listItems = Array.from(document.querySelectorAll('li')).filter(el => el.querySelector('img') && !el.querySelector('p'))
+        let res = []
+        for (let item of listItems) {
+            let src = item.querySelector('img').src
+            if (src.match("card/render/")) {
+                //User has only 1 of that card, so the img link contains the templateId
+                let templateId = parseInt(src.match(/(\d+)/g)[1])
+                res.push({
+                    item: item,
+                    templateId: templateId
+                })
+            } else {
+                let template = data.data.find(elem => {
+                    return elem.images.size402 === src
+                })
+                let templateId = template.id
+                res.push({
+                    item: item,
+                    templateId: templateId
+                })
             }
-        })
-    })
-
-    observer.observe(document.body, {
-        childList: true,
-        subtree: true,
-        attributes: false,
-        characterData: false
+        }
+        getMarketValues(res, categoryId, jwt)
     })
 }
 
-function getMarketValues(card) {
-    console.log(card)
+function getMarketValues(cards, categoryId, jwt) {
+    for (let card of cards) {
+        let url = `https://api.epics.gg/api/v1/market/buy?categoryId=${categoryId}&page=1&sort=price&templateId=${card.templateId}&type=card`
+        fetch(url, {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+              'X-User-JWT': jwt
+            },
+        }).then(res => res.json()).then(data => {
+            card.item.style.display = "block"
+            let price = null
+            if (data.data.count > 0) {
+                price = data.data.market[0][0].price
+            } else {
+                price = "-"
+            }
+            let h2 = document.createElement("H2")
+            h2.style.marginTop = "10px"
+            h2.innerHTML = price
+            card.item.appendChild(h2)
+        })
+    }
 }
