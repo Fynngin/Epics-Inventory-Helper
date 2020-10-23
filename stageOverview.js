96b22f8bb3527c0761c8c1befc4a5fe602ce117e
Fynn Knoop <fknoop@techfak.de>
2020-10-23 18:14:29 +0200

    Added stage progress

--- stageOverview.js
+++ stageOverview.js
@@ -1,5 +1,6 @@
 let stages = []
 let teams = []
+let stagesCompleted = 0
 
 chrome.runtime.onMessage.addListener(
     function(request) {
@@ -41,6 +42,7 @@ function removeRushAds() {
 
 async function addStages(callback) {
     if (stages.length > 0) {
+        removeRushAds()
         addDomElements(callback)
     } else {
         await fetch("https://api.epics.gg/api/v1/ultimate-team/circuits/35", {
@@ -52,6 +54,7 @@ async function addStages(callback) {
         }).then(res => res.json()).then(async data => {
             removeRushAds()
             let circuit = data.data['circuit']
+            stagesCompleted = circuit['stagesCompleted']
             for (const stage of circuit['stages']) {
                 let ids = JSON.stringify(stage['rosters'].map(roster => roster['ut_pve_roster_id']))
                 await fetch(`https://api.epics.gg/api/v1/ultimate-team/pve/rosters?categoryId=1&ids=${ids}`, {
@@ -64,16 +67,18 @@ async function addStages(callback) {
                     let tempTeams = []
                     for (const team of data.data['rosters']) {
                         let logo = teams.find(el => el.id === team.teamId).images.find(im => im.name === "team_logo").url
+                        let progress = stage['rosterProgress'].find(el => el['ut_pve_roster_id'] === team.id)['wins']
                         tempTeams.push({
                             name: team.name,
                             rating: team.rating,
                             teamId: team.teamId,
                             rosterId: team.id,
-                            progress: stage['rosterProgress'].find(el => el['ut_pve_roster_id'] === team.id)['wins'],
+                            progress: progress >= stage['rosters'][0]['wins'] ? stage['rosters'][0]['wins'] : progress,
                             logo: `https://cdn.epics.gg${logo}`
                         })
                     }
                     stages.push({
+                        badge: `https://cdn.epics.gg${stage['images'][0].url}`,
                         teams: tempTeams,
                         order: circuit['allStages'].find(el => el.id === stage.id).order,
                         name: stage.name,
@@ -94,6 +99,7 @@ async function addDomElements(callback) {
     main.appendChild(aside)
 
     for (const stage of stages) {
+        stage.isShowing = false
         let section = document.createElement('section')
         section.style.paddingLeft = "10px"
         section.style.paddingRight = "10px"
@@ -105,21 +111,42 @@ async function addDomElements(callback) {
             .then(res => res.text())
             .then(async data => {
                 section.innerHTML = data
+                let badge = section.querySelector('header img')
+                badge.src = stage.badge
                 let header = section.querySelector('header h2')
                 header.innerHTML = stage['name']
-                header.parentElement.parentElement.addEventListener('click', handleStageClick)
+                if (stage.order <= (stagesCompleted + 1)) {
+                    header.parentElement.parentElement.addEventListener('click', handleStageClick)
+                }
                 let innerDiv = section.querySelector('.inner_div')
                 for (const team of stage.teams) {
                     let li = document.createElement('li')
-                    let div = document.createElement('div')
+                    li.style.cursor = "pointer"
+                    li.addEventListener('click', function() {
+                        handleTeamClick(team)
+                    })
+                    let flex = document.createElement('div')
+                    flex.style.width = "100%"
+                    flex.style.display = "flex"
+                    flex.style.justifyContent = "space-between"
+                    let div_right = document.createElement('div')
+                    div_right.style.alignSelf = "center"
+                    div_right.style.marginLeft = "auto"
+                    let progress = document.createElement('h5')
+                    progress.innerHTML = `${team.progress} / ${stage.winsNeeded}`
+                    progress.style.color = team.progress >= stage.winsNeeded ? "#0baaaa" : "#aa0b53"
+                    div_right.appendChild(progress)
+                    let div_left = document.createElement('div')
                     let p = document.createElement('p')
                     let img = document.createElement('img')
                     img.src = team.logo
                     img.style.height = "30px"
                     p.innerHTML = team.name
-                    li.appendChild(div)
-                    div.appendChild(img)
-                    div.appendChild(p)
+                    div_left.appendChild(img)
+                    div_left.appendChild(p)
+                    flex.appendChild(div_left)
+                    flex.appendChild(div_right)
+                    li.appendChild(flex)
                     team.container = innerDiv
                     team.item = li
                 }
@@ -143,4 +170,8 @@ function handleStageClick(evt) {
         }
         stage.isShowing = true
     }
+}
+
+function handleTeamClick(team) {
+    console.log(team)
 }
